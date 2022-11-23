be4d0b697d1c2f87bf25b4c31568b153efc809e7
yerakulmanov <200103127@stu.sdu.edu,.kz>
2022-11-23 23:51:32 +0600

    homefragment, fragment xml, themes, svg icons

--- app/src/main/java/com/example/teamproject/MainActivity.kt
+++ app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.teamproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+import com.example.teamproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    //        private lateinit var binding: ActivityMainBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+//            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(R.layout.activity_main)
+
+            replaceFragment(HomeFragment.newInstance(),true)
+    }
+
+    fun replaceFragment(fragment: Fragment, isTransition:Boolean){
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+
+        if(isTransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 }
