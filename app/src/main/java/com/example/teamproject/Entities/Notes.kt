e99a2d549cfd3050aa0f57394ea861a47019e212
zhangir <samsung240446@gmail.com>
2022-11-23 15:41:27 +0600

    add Data class

---
+++ app/src/main/java/com/example/teamproject/Entities/Notes.kt
@@ -0,0 +1,30 @@
+package com.example.teamproject.Entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName= "Notes")
+data class Notes (
+    @PrimaryKey (autoGenerate = true)
+var id: Int,
+
+    @ColumnInfo(name = "title")
+var title:String,
+E. I: Structure
+    @ColumnInfo(name = "sub_title")
+var subTitle: String,
+  @ColumnInfo(name - "date_time")
+var datelime:String,
+  @ColumnInfo(name = "note_text")
+var noteText: String,
+ @ColumnInfo(name = "img_path")
+var ImgPath:String,
+@ColumnInfo(name = "web_link")
+var weblink:String,
+@ColumnInfo(name = "color")
+var color:String
+
+){
+    override fun tostring(): string (
+    return "$title : $dateTime"
+}
