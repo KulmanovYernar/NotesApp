adaa12e0b93d1cc965b2676aa6c22f72f418e958
Adilbek Karimov <berikka10@gmail.com>
2022-11-27 13:29:28 +0600

    Updated details for set text

--- app/src/main/java/com/example/teamproject/CreateNoteFragment.kt
+++ app/src/main/java/com/example/teamproject/CreateNoteFragment.kt
@@ -1,12 +1,20 @@
 package com.example.teamproject
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class CreateNoteFragment : Fragment() {
+import android.widget.Toast
+import com.example.teamproject.database.NotesDatabase
+import com.example.teamproject.entities.Notes
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
+class CreateNoteFragment : BaseFragment() {
+    var currentDate:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -30,5 +38,59 @@ class CreateNoteFragment : Fragment() {
 
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        currentDate = sdf.format(Date())
+
+        tvDateTime.text = curretDate
+
+        imgDone.setOnClickListener {
+            //saveNote
+            saveNote()
+        }
+
+        imgBack.setOnClickListener {
+            replaceFragment(HomeFragment.newInstance(), false)
+        }
+    }
+
+    private fun saveNote()
+    {
+        if (etNoteTitle.text.isNullOrEmpty()){
+            Toast.makeText(context, "Note Title is Required", Toast.LENGTH_SHORT).show()
+        }
+        if (etNoteSubTitle.text.isNullOrEmpty()){
+            Toast.makeText(context, "Note Sub Title is Required", Toast.LENGTH_SHORT).show()
+        }
+        if (etNoteDesc.text.isNullOrEmpty()){
+            Toast.makeText(context, "Note Description is Required", Toast.LENGTH_SHORT).show()
+        }
+        launch {
+            var  notes = Notes()
+            notes.title = etNoteTitle.text.toString()
+            notes.subTitle = etNoteSubTitle.text.toString()
+            notes.noteText = etNoteDesc.text.toString()
+            notes.dateTime = currentDate
+            context?.let {
+                NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
+                etNoteTitle.setText("")
+                etNoteSubTitle.setText("")
+                etNoteDesc.setText("")
+            }
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment, istransition: Boolean)
+    {
+        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+
+        if (istransition)
+        {
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frame_layout, fragment).addToBackStack(fragment.javaClass.simpleName)
     }
 }
