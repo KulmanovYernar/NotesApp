6db1b8ce967438230f0bd4656c883643377d9b8f
yerakulmanov <200103127@stu.sdu.edu,.kz>
2022-11-27 15:31:01 +0600

    Fixed bugs and add Kotlin extension dependencies

--- app/src/main/java/com/example/teamproject/HomeFragment.kt
+++ app/src/main/java/com/example/teamproject/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
+
 class HomeFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,18 +37,18 @@ class HomeFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fabBtnCreateNote.setOnClickListener {
-            replaceFragment(CreateNoteFragment.newInstance(). false)
+            replaceFragment(CreateNoteFragment.newInstance(), false)
         }
     }
     fun replaceFragment(fragment: Fragment, istransition: Boolean)
     {
-        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+        val fragmentTransition = requireActivity().supportFragmentManager.beginTransaction()
 
         if (istransition)
         {
             fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
         }
-        fragmentTransition.replace(R.id.frame_layout, fragment).addToBackStack(fragment.javaClass.simpleName)
+        fragmentTransition.replace(R.id.frame_layout, fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 
 }
--- app/src/main/java/com/example/teamproject/dao/NoteDao.kt
+++ app/src/main/java/com/example/teamproject/dao/NoteDao.kt
@@ -1,13 +1,16 @@
 package com.example.teamproject.dao
 
 import androidx.room.*
-import com.codingwithme.notesapp.entities.Notes
+import com.example.teamproject.entities.Notes
+
 @Dao
 interface NoteDao{
-    @get: Query( value: "SELECT * FROM notes ORDER BY id DESC")
+    @get: Query("SELECT * FROM notes ORDER BY id DESC")
     val allNotes: List<Notes>
-    @InsertonConflict = OnConflictStrategy.REPLACE)
-    fun insertNotes(note:Notes)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNotes(note:Notes)
+
     @Delete
-    fun deleteNote(note:Notes )
+    suspend fun deleteNote(note:Notes)
 }
