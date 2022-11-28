b9969731470b06fdb00d5c09392e02e7157885d6
yerakulmanov <200103127@stu.sdu.edu,.kz>
2022-11-28 14:57:54 +0600

    add NoteBottomSheet fragment and xml, notes xml files for choosing a color

---
+++ app/src/main/java/com/example/teamproject/util/NoteBottomSheetFragment.kt
@@ -0,0 +1,85 @@
+package com.example.teamproject.util
+
+import android.app.Dialog
+import android.content.Intent
+import android.os.Bundle
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.teamproject.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_notes_bottom_sheet.*
+
+class NoteBottomSheetFragment: BottomSheetDialogFragment() {
+
+    override fun setupDialog(dialog: Dialog, style: Int) {
+        super.setupDialog(dialog, style)
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_notes_bottom_sheet,null)
+        dialog.setContentView(view)
+
+        val param = (view.parent as View).layoutParams as CoordinatorLayout.LayoutParams
+
+        val behavior = param.behavior
+
+        if(behavior is BottomSheetBehavior<*>){
+            behavior.setBottomSheetCallback(object:BottomSheetBehavior.BottomSheetCallback(){
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    TODO("Not yet implemented")
+                }
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    var state = ""
+                    when (newState) {
+                        BottomSheetBehavior.STATE_DRAGGING -> {
+                            state = "DRAGGING"
+                        }
+                        BottomSheetBehavior.STATE_SETTLING -> {
+                            state = "SETTLING"
+                        }
+                        BottomSheetBehavior.STATE_EXPANDED -> {
+                            state = "EXPANDED"
+                        }
+                        BottomSheetBehavior.STATE_COLLAPSED -> {
+                            state = "COLLAPSED"
+                        }
+
+                        BottomSheetBehavior.STATE_HIDDEN -> {
+                            state = "HIDDEN"
+                            dismiss()
+                            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                        }
+                    }
+                }
+
+
+
+            })
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_notes_bottom_sheet,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListener()
+    }
+
+    private fun setListener(){
+        fNote1.setOnClickListener {
+            val intent = Intent("bottom_sheet_action")
+            intent.putExtra("actionBlue","Blue")
+            LocalBroadcastManager.getInstance(requireContext()).sendBroadcast(intent)
+            dismiss()
+        }
+    }
+}
+
